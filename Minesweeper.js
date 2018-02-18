4ba3968650f2ee0cf947cf04c78491ad869e6a02
Marcus Polk <marcuspolk96@gmail.com>
2018-02-18 08:34:19 -0800

    Organized functions. Only moved, no change in code

--- Minesweeper.js
+++ Minesweeper.js
@@ -79,30 +79,6 @@ class Minesweeper {
     return position;
   }
 
-  async init() {
-    this.mines = [];
-    const difficulties = { E: [9, 9, 10], M: [16, 16, 40], H: [30, 16, 99] };
-    const difficultyMessage = 'Please enter a difficulty: E, M, H.';
-    // TODO: handle custom difficulty.
-    let difficulty;
-    let difficultyChar;
-    const setDifficultyChar = function (userInput) {
-      difficultyChar = userInput.charAt(0).toUpperCase();
-    };
-    do {
-      await getUserInput(difficultyMessage)
-        .then(setDifficultyChar);
-      difficulty = difficulties[difficultyChar];
-    } while (difficulty === undefined);
-    [this.width, this.height, this.bombCount] = difficulty;
-    this.boardSize = this.width * this.height;
-    this.leftToVisit = this.boardSize - this.bombCount;
-
-    for (let i = 0; i < this.boardSize; i += 1) {
-      this.mines[i] = 0;
-    }
-    this.genBombs();
-  }
 
   isOnBoard(pos) {
     return (pos >= 0 && pos < this.boardSize);
@@ -118,61 +94,6 @@ class Minesweeper {
     this.gameInProgress = false;
     this.draw();
     console.log('Game over! You detonated a bomb!');
-  }
-
-  async makeFirstMove() {
-    let position;
-    do {
-      const tempPosition = await this.getValidMove();
-      if (this.isOnBoard(tempPosition)) {
-        position = tempPosition;
-      } else {
-        console.log('You cannot set a flag as first move.');
-      }
-    } while (position === undefined);
-
-    if (this.isBomb(position)) {
-      const newBombLocation = this.findBomblessCell();
-      this.placeBomb(newBombLocation);
-      this.removeBomb(position);
-    }
-    this.explore(position);
-  }
-
-
-  isPlayable(pos) {
-    const cell = this.mines[pos];
-    return (cell >= 0 && cell < 10);
-  }
-
-  isAllClear(pos) {
-    return this.mines[pos] === 0;
-  }
-
-  isBomb(pos) {
-    return (this.mines[pos] === 9);
-  }
-
-  isVisited(pos) {
-    return (this.mines[pos] >= 10);
-  }
-
-  isFlagged(pos) {
-    return (this.mines[pos] < 0);
-  }
-
-  toggleFlag(pos) {
-    this.isFlagged(pos) ? this.unflag(pos) : this.flag(pos);
-  }
-
-  flag(pos) {
-    this.mines[pos] -= 10;
-    this.flagCount += 1;
-  }
-
-  unflag(pos) {
-    this.mines[pos] += 10;
-    this.flagCount -= 1;
   }
 
   explore(pos) {
@@ -204,6 +125,36 @@ class Minesweeper {
     this.leftToVisit -= 1;
   }
 
+  isVisited(pos) {
+    return (this.mines[pos] >= 10);
+  }
+
+  isAllClear(pos) {
+    return this.mines[pos] === 0;
+  }
+
+  isBomb(pos) {
+    return (this.mines[pos] === 9);
+  }
+
+  isFlagged(pos) {
+    return (this.mines[pos] < 0);
+  }
+
+  toggleFlag(pos) {
+    this.isFlagged(pos) ? this.unflag(pos) : this.flag(pos);
+  }
+
+  flag(pos) {
+    this.mines[pos] -= 10;
+    this.flagCount += 1;
+  }
+
+  unflag(pos) {
+    this.mines[pos] += 10;
+    this.flagCount -= 1;
+  }
+
   getNeighbors(pos) {
     const neighbors = [];
     const w = this.width;
@@ -218,53 +169,6 @@ class Minesweeper {
     neighbors.push(pos + w + 1);
 
     return neighbors.filter(cell => this.isOnBoard(cell));
-  }
-
-  coordToI(coordinate) {
-    // takes coordinate string, converts it to corresponding index.
-    const rowChar = coordinate.charAt(0).toUpperCase();
-    const col = parseInt(coordinate.slice(1), 10);
-
-    if (typeof rowChar !== 'string') return -1;
-    if (isNaN(col)) return -1;
-
-    const rowNum = rowChar.charCodeAt() - 65;
-    return (rowNum * this.width) + col;
-  }
-
-  iToCoord(pos) {
-    // w: 20
-    // ex: pos: 15 => a15
-    // pos: 46 => c6
-    const w = this.width;
-    const charNum = Math.floor(pos / w);
-    const colNum = pos - (charNum * w);
-    const char = String.fromCharCode(charNum + 65);
-    const colString = colNum < 10 ? ` ${colNum}` : `${colNum}`;
-    return `${char}${colString}`;
-  }
-
-  draw() {
-    // iterate through mine array.
-    // if isExplored(mine[i]), draw mine[i] - 10.
-    // else if isFlagged(mine[i]), draw F.
-    // else draw blank.
-    console.log(`Bombs: ${this.bombCount - this.flagCount} Time: TODO Clicks: ${this.moveCount}`);
-    let position = 0;
-    for (let i = 0; i < this.height; i += 1) {
-      // print the row.
-      const row = [];
-      for (let j = 0; j < this.width; j += 1) {
-        let char;
-        if (this.isVisited(position)) char = ` ${this.mines[position] - 10} `;
-        else if (this.isFlagged(position)) char = ' F ';
-        else if (this.gameInProgress) char = `${this.iToCoord(position)}`;
-        else char = ' X ';
-        row.push(char);
-        position += 1;
-      }
-      console.log(`| ${row.join(' | ')} |`);
-    }
   }
 
   genBombs() {
@@ -301,6 +205,96 @@ class Minesweeper {
         if (this.isBomb(cell)) return neighborBombCount + 1;
         return neighborBombCount;
       }, 0);
+  }
+  draw() {
+    // iterate through mine array.
+    // if isExplored(mine[i]), draw mine[i] - 10.
+    // else if isFlagged(mine[i]), draw F.
+    // else draw blank.
+    console.log(`Bombs: ${this.bombCount - this.flagCount} Time: TODO Clicks: ${this.moveCount}`);
+    let position = 0;
+    for (let i = 0; i < this.height; i += 1) {
+      // print the row.
+      const row = [];
+      for (let j = 0; j < this.width; j += 1) {
+        let char;
+        if (this.isVisited(position)) char = ` ${this.mines[position] - 10} `;
+        else if (this.isFlagged(position)) char = ' F ';
+        else if (this.gameInProgress) char = `${this.iToCoord(position)}`;
+        else char = ' X ';
+        row.push(char);
+        position += 1;
+      }
+      console.log(`| ${row.join(' | ')} |`);
+    }
+  }
+
+  coordToI(coordinate) {
+    // takes coordinate string, converts it to corresponding index.
+    const rowChar = coordinate.charAt(0).toUpperCase();
+    const col = parseInt(coordinate.slice(1), 10);
+
+    if (typeof rowChar !== 'string') return -1;
+    if (isNaN(col)) return -1;
+
+    const rowNum = rowChar.charCodeAt() - 65;
+    return (rowNum * this.width) + col;
+  }
+
+  iToCoord(pos) {
+    // w: 20
+    // ex: pos: 15 => a15
+    // pos: 46 => c6
+    const w = this.width;
+    const charNum = Math.floor(pos / w);
+    const colNum = pos - (charNum * w);
+    const char = String.fromCharCode(charNum + 65);
+    const colString = colNum < 10 ? ` ${colNum}` : `${colNum}`;
+    return `${char}${colString}`;
+  }
+  
+  async makeFirstMove() {
+    let position;
+    do {
+      const tempPosition = await this.getValidMove();
+      if (this.isOnBoard(tempPosition)) {
+        position = tempPosition;
+      } else {
+        console.log('You cannot set a flag as first move.');
+      }
+    } while (position === undefined);
+
+    if (this.isBomb(position)) {
+      const newBombLocation = this.findBomblessCell();
+      this.placeBomb(newBombLocation);
+      this.removeBomb(position);
+    }
+    this.explore(position);
+  }
+
+  async init() {
+    this.mines = [];
+    const difficulties = { E: [9, 9, 10], M: [16, 16, 40], H: [30, 16, 99] };
+    const difficultyMessage = 'Please enter a difficulty: E, M, H.';
+    // TODO: handle custom difficulty.
+    let difficulty;
+    let difficultyChar;
+    const setDifficultyChar = function (userInput) {
+      difficultyChar = userInput.charAt(0).toUpperCase();
+    };
+    do {
+      await getUserInput(difficultyMessage)
+        .then(setDifficultyChar);
+      difficulty = difficulties[difficultyChar];
+    } while (difficulty === undefined);
+    [this.width, this.height, this.bombCount] = difficulty;
+    this.boardSize = this.width * this.height;
+    this.leftToVisit = this.boardSize - this.bombCount;
+
+    for (let i = 0; i < this.boardSize; i += 1) {
+      this.mines[i] = 0;
+    }
+    this.genBombs();
   }
 }
 
